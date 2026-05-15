616ceccb123477a8ae46c57198f36cd91cc39cd8
George <remcity36@ya.ru>
2026-05-15 21:47:13 +0300

    Delete analytics.js

--- analytics.js
+++
@@ -1,5 +0,0 @@
-/* Vercel Web Analytics - Vanilla Implementation */
-import { inject } from './node_modules/@vercel/analytics/dist/index.mjs';
-
-// Initialize Web Analytics for vanilla static site
-inject();
