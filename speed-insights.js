64c3ccc7e22c1f9cd9a353a75eda1a3c447c2ad7
George <remcity36@ya.ru>
2026-05-15 21:48:29 +0300

    Delete speed-insights.js

--- speed-insights.js
+++
@@ -1,5 +0,0 @@
-/* Vercel Speed Insights - Vanilla Implementation */
-import { injectSpeedInsights } from './node_modules/@vercel/speed-insights/dist/index.mjs';
-
-// Initialize Speed Insights for vanilla static site
-injectSpeedInsights();
